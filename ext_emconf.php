a10b22727847759611613ef0da1633ca821f1af2
Björn <b.heggemann@neonaut.de>
2017-08-22 16:26:14 +0200

    [TASK] expand dependencies

--- ext_emconf.php
+++ ext_emconf.php
@@ -36,7 +36,7 @@ $EM_CONF[$_EXTKEY] = array(
         array(
             'depends' =>
                 array(
-                    'typo3' => '6.2.0-6.2.99',
+                    'typo3' => '6.2.0-7.6.99',
                     'news' => '5.3.0-6.0.99'
                 ),
             'conflicts' =>
