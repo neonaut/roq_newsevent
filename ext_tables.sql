c417bbd3bffbba908d131e00c62ed1492f03bcad
Björn <b.heggemann@neonaut.de>
2017-09-25 10:11:27 +0200

    [TASK] Change default value

--- ext_tables.sql
+++ ext_tables.sql
@@ -3,7 +3,7 @@
 #
 CREATE TABLE tx_news_domain_model_news (
 
-  tx_roqnewsevent_is_event  TINYINT(1) UNSIGNED DEFAULT '0' NOT NULL,
+  tx_roqnewsevent_is_event  TINYINT(1)              NOT NULL DEFAULT '0',
   tx_roqnewsevent_startdate INT(11) DEFAULT '0'             NOT NULL,
   tx_roqnewsevent_starttime INT(11) DEFAULT '0'             NOT NULL,
   tx_roqnewsevent_enddate   INT(11) DEFAULT '0'             NOT NULL,
